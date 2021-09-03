d7edee8292d724d000c529fd5dc253f03561bfc8
khan-07 <uzair_93cool@hotmail.com>
2021-09-03 15:52:59 +0100

    Solution without division for Problem -2.

--- p2-hard.py
+++ p2-hard.py
@@ -6,10 +6,12 @@
 #
 # Follow-up: what if you can't use division?
 
+# An obvious solution is O(n^2)
+
 def p2hard(array):
     product = 1
     for item in array:
-        product = product * item
+        product *= item
 
     for idx, val in enumerate(array):
         array[idx] = product / array[idx]
@@ -17,8 +19,39 @@ def p2hard(array):
     return array
 
 
+def p2hard_sol2(array):
+
+    forward= [1] * len(array)
+    backward = [1] * len(array)
+    forward[0] = array[0]
+    product = [1] * len(array)
+    for idx, val in enumerate(array):
+        if idx == 0:
+            continue
+        else:
+            forward[idx] = array[idx] * forward[idx-1]
+
+    backward[len(array) -1] = array[len(array) - 1]
+    for idx,val in reversed(list(enumerate(array))):
+        if idx == len(array)-1:
+            continue
+        else:
+            backward[idx] = array[idx] * backward[idx + 1]
+
+    for idx,val in enumerate(array):
+        if idx == 0:
+            product[0] = backward[idx +1]
+        elif idx == len(array) -1:
+            product[idx] = forward[len(array) - 2]
+        else:
+            product[idx] = forward[idx-1] * backward[idx+1]
+    return product,backward,forward
+
+
+
 print(p2hard([1, 2, 3, 4, 5]))
 print(p2hard([3,2,1]))
 print(p2hard([]))
-
+print(p2hard_sol2([3,2,1]))
+print(p2hard_sol2([1,2,3,4,5]))
 # Solution is O(n)
